5df832488c8208cf419ba4f6f7545c300ac63dbd
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-17 19:42:08 +0530

    Loader added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,39 @@
+import { useProgress } from "@react-three/drei";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+
+const Loader = ({ onComplete }) => {
+  const { progress } = useProgress();
+  const loaderRef = useRef();
+  const barRef = useRef();
+
+  useEffect(() => {
+    gsap.to(barRef.current, {
+      width: `${progress}%`,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+
+    if (progress === 100) {
+      gsap.to(loaderRef.current, {
+        opacity: 0,
+        duration: 1,
+        ease: "power3.inOut",
+        onComplete: onComplete,
+      });
+    }
+  }, [progress]);
+  return(
+    <div ref={loaderRef} className="loader">
+      <div className="loader-inner">
+        <h1>DogStudio</h1>
+        <div className="progress-track">
+          <div ref={barRef} className="progress-bar" />
+        </div>
+        <p>{Math.floor(progress)}%</p>
+      </div>
+    </div>
+  )
+};
+
+export default Loader;
